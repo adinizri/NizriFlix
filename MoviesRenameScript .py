71b2e29dcc029e05f6cd576c5322a8413decd142
Adi Nizri <adin624@gmail.com>
2022-01-16 20:08:16 +0200

    python scripts

---
+++ MoviesRenameScript .py
@@ -0,0 +1,45 @@
+import os 
+
+dirlocation='G:/Movies&Series/Movies/all'
+for dirpath,dirnames,filenames in os.walk('G:/Movies&Series/Movies/all'):
+    print(filenames )
+    names=filenames
+
+    for name in names:
+         newName=name
+         if 'TorrentPart' in newName:
+             os.remove(dirpath+os.sep+name)
+         else:
+            newName=newName.replace("."," ")
+            
+            if 'mp4' in newName:
+                newName=newName.replace("mp4","mp")
+                newName=newName.replace("mp","")
+                if '1080p'in newName:
+                  newName=newName.replace((newName[newName.index('1080p'):len(newName)-1]),"") 
+                if '720p'in newName:
+                  newName=newName.replace((newName[newName.index('720p'):len(newName)-1]),"") 
+                if(' 4 ' in newName) :
+                  newName=newName.replace(" 4 ","")
+                newName+=".mp4"
+         
+            elif '4' == newName[-1] and not 'str' in newName:
+                newName=newName.replace("4",".mp4")
+                
+           
+            if 'Sub' in dirpath:
+               
+               if '1080p'in newName:
+                    newName=newName.replace((newName[newName.index('1080p'):len(newName)-1]),"") 
+                    newName+='-heb.srt' 
+               elif '720p'in newName:
+                    newName=newName.replace((newName[newName.index('720p'):len(newName)-1]),"") 
+                    newName+='-heb.srt' 
+               else:
+                   newName=newName.replace('-heb srt' ,"")  
+                   if not('-heb.srt' in newName):
+                         newName+='-heb.srt'
+            newName=newName.replace("  ","")        
+            os.rename(dirpath+os.sep+name,dirpath+os.sep+ newName) 
+  
+
--- dbUpdater.py
+++ dbUpdater.py
@@ -1,3 +1,31 @@
 import pymongo
-client = pymongo.MongoClient("localhost", 27017)
-db = client.Movies
+import os
+client = pymongo.MongoClient("localhost", 27017)  # mongo server address
+db = client["nizriFlix"]  # db name
+dbCol = db["Movies"]  # db collection
+dirlocation = 'G:/Movies&Series/Movies/all'  # source
+movieList = []
+
+for dirpath, dirnames, filenames in os.walk(dirlocation):
+    # print(filenames)
+    names = filenames
+
+    for name in names:
+        image = ""
+        if 'mp4' in name:
+            newName = name.replace('.mp4', "")
+            for img in names:
+
+                fixImage = img.replace('_', " ")
+
+                if ('.png' in img or '.jfif' in img) and newName in fixImage:
+                    image = img
+
+            dirpath = dirpath.replace("\\", "/")
+            img = "G:/Movies&Series/Movies/Empty_Img.png"
+            obj = {"name": newName, "location": dirpath, "img": image}
+            if image != "":
+                movieList.append(obj)
+
+dbCol.insert_many(movieList)  # add to collection
+# dbCol.delete_many({})  # clear collection
