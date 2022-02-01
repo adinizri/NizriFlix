52048961397a2d861822dbfd134bfaa3b3027dee
Adi Nizri <adin624@gmail.com>
2022-02-01 18:51:58 +0200

    adding nav

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,48 +1,59 @@
 import logo from './logo.svg';
 import './App.scss';
+import miniLogo from '../src/Images/NizriFlix_Mini_Logo.png';
 import { createContext, useState, useEffect, useContext, useMemo } from 'react';
-import CarouselContainer from './Components/SharedComponents/CarouselContainer/CarouselContainer';
-import Logo from './Components/SharedComponents/Logo/Logo';
-import Player from './Components/SharedComponents/Player/Player';
-import { Router } from 'react-router-dom';
-import { videoData } from './Contexts/Contexts';
 
+import Logo from './Components/SharedComponents/Logo/Logo';
+import Movies from './Components/Pages/Movies/Movies';
+import { videoData } from './Contexts/Contexts';
+import ReactDOM from "react-dom";
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 const App = () => {
-  const [playing, setPlaying] = useState(false);
-  const [data, setData] = useState();
+
+  return (
+
+    <Router>
+
+      <div className='appContainer'>
+
+
+        <ul >
+          <il >
+            <Link to="/HomePage"><img src={ miniLogo }></img></Link>
+
+          </il>
+          <li>
+            <Link to="/Movies">Movies</Link>
+          </li>
+          <li>
+            <Link to="/HomePage">HomePage</Link>
+          </li>
+          <li>
+            <Link to="/Series">Series</Link>
+          </li>
+
+        </ul>
+        <Routes>
+          <Route exact path='/Movies' element={ < Movies /> }></Route>
+
+        </Routes>
+
+
+      </div>
+    </Router >
 
 
 
+  );
+};
 
-  // const isPlaying = () => {
-  //   setPlaying(true);
-  // };
-  // const getData = (data) => {
-  //   setData(data);
-  // };
-  return (<>
-    <Logo></Logo>
-    {/* <Router>
-      <nav>
-        <Link to="/Movies">My Profile</Link>
-        <br />
-        <Link to={ `/profile/ann` }>Ann's Profile</Link>
-      </nav>
-      <Route path="/profile">
-        <Profile />
-      </Route>
-    </Router> */}
-
-    <div className='appContainer'>
+export default App;
+{/* <div className='appContainer'>
 
       { playing ? <Player data={ data } ></Player> :
         <videoData.Provider value={ { setPlaying: setPlaying, setData: setData } }><CarouselContainer ></CarouselContainer> </videoData.Provider> }
 
 
-    </div>
-  </>
-  );
-};
-
-export default App;
+    </div> */}
--- Client/src/Components/Pages/Movies/Movies.js
+++ Client/src/Components/Pages/Movies/Movies.js
@@ -1,1 +1,17 @@
-import CarouselContainer from './Components/SharedComponents/CarouselContainer/CarouselContainer';
+import CarouselContainer from '../../SharedComponents/CarouselContainer/CarouselContainer';
+import Player from '../../SharedComponents/Player/Player';
+import { videoData } from '../../../Contexts/Contexts';
+import { createContext, useState, useEffect, useContext, useMemo } from 'react';
+const Movies = () => {
+    const [playing, setPlaying] = useState(false);
+    const [data, setData] = useState();
+    return (
+        <>
+            { playing ? <Player data={ data }></Player > :
+                <videoData.Provider value={ { setPlaying: setPlaying, setData: setData } }><CarouselContainer ></CarouselContainer> </videoData.Provider> }
+        </>
+
+
+    );
+};
+export default Movies;
